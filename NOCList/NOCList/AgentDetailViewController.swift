3a482c55cf30d796016211fce4320a02c48b8686
Heather Langager <heatherlangager@Heathers-Air.domain>
2019-05-29 16:30:21 -0700

    Updated Project

---
+++ NOCList/NOCList/AgentDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AgentDetailViewController.swift
+//  NOCList
+//
+//  Created by Heather Langager on 5/29/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AgentDetailViewController: UIViewController {
+    
+    @IBOutlet weak var CoverName: UILabel!
+    @IBOutlet weak var RealName: UILabel!
+    @IBOutlet weak var AccessLevel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- NOCList/NOCList/NOCListTableViewController.swift
+++ NOCList/NOCList/NOCListTableViewController.swift
@@ -8,24 +8,33 @@
 
 import UIKit
 
-class NOCListTableViewController: UITableViewController
-{
+class NOCListTableViewController: UITableViewController {
+    
+    private var agents: [(coverName: String, realName: String, accesslevel: Int, compromised: Bool)] = []
+
     // MARK: - Properties
     
     // MARK: - View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadNOCList()
     }
     
-    // MARK: - Table view data source
+    // MARK: - Table view data source 
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return agents.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CoverNameCell", for: indexPath)
+        let CoverName = agents[indexpath.row]
+        cell.textLabel.text = agentsItem.name
+        cell.detailTextLabel.text
+        
+        
+        
         
         return cell
     }
@@ -37,4 +46,30 @@ class NOCListTableViewController: UITableViewController
     }
     
     // MARK: - Private
+    
+    private func loadNOCList()
+    {
+        let ethan = (coverName: "Ethan Hunt", realName: "Tom Cruise", accessLevel: 8, compromised: true)
+        let jim = (coverName: "Jim Phelps", realName: "Jon Voight", accessLevel: 9, compromised: false)
+        let claire = (coverName: "Claire Phelps", realName: "Emmanuelle Beart", accessLevel: 5, compromised: false)
+        let eugene = (coverName: "Eugene Kittridge", realName: "Henry Czerny", accessLevel: 10, compromised: true)
+        let franz = (coverName: "Franz Krieger", realName: "Jean Reno", accessLevel: 4, compromised: false)
+        let luther = (coverName: "Luther Stickell", realName: "Ving Rhames", accessLevel: 4, compromised: false)
+        let sarah = (coverName: "Sarah Davies", realName: "Kristin Scott Thomas", accessLevel: 5, compromised: true)
+        let max = (coverName: "Max RotGrab", realName: "Vanessa Redgrave", accessLevel: 4, compromised: false)
+        let hannah = (coverName: "Hannah Williams", realName: "Ingeborga Dapkūnaitė", accessLevel: 5, compromised: true)
+        let jack = (coverName: "Jack Harmon", realName: "Emilio Estevez", accessLevel: 6, compromised: true)
+        let frank = (coverName: "Frank Barnes", realName: "Dale Dye", accessLevel: 9, compromised: false)
+        
+        agents.append(contentsOf: [ethan, jim, claire, eugene, franz, luther, sarah, max, hannah, jack, frank])
+    }
+    
+    private func compromiseCount() -> Int {
+        var count = 0
+        for agent in agents {
+            if compromised == true
+            count += 1
+        }
+    }
+    return
 }
